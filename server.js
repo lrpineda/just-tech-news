f2ac32ea8c10892b2cc9d68c0e21dafa6b2294d7
lrpineda <luicks212@gmail.com>
2022-06-25 10:08:56 -0600

    fixing bugs

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const express = require('express');
-const helpers = require('./utils/helpers');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers});
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -20,17 +19,18 @@ const sess = {
   })
 };
 
+app.use(session(sess));
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const helpers = require('./utils/helpers');
+const routes = require('./controllers');
+const hbs = exphbs.create({ helpers});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(session(sess));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // turn on routes
 app.use(routes);
