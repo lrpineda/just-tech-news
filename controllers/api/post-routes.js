2b0089e82601953c287df58cf1ee9fc72d446681
lrpineda <luicks212@gmail.com>
2022-06-24 23:11:08 -0600

    handlebars stuff

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -86,13 +86,16 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => { 
-   // custom static method created in models/post.js
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    // make sure the session exists
+    if(req.session){
+        // pass session id along with all destructured properties
+        Post.upvote({...req.body, user_id: req.session.user_id}, {Vote,Comment,User})
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
 });
 
 router.put('/:id', (req, res) => { 
