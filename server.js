134917e5206cbfc444eac3d8eb3b577308f3f0d3
lrpineda <luicks212@gmail.com>
2022-06-25 00:54:41 -0600

    handlebars partials

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
+const helpers = require('./utils/helpers');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers});
 const session = require('express-session');
+
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
